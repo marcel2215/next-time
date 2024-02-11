165a0a94517f652022975490154717071a3ab52f
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-02-11 01:55:25 +0100

    Add Choice entity class

---
+++ src/Entities/Choice.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace NextTime.Entities;
+
+public sealed class Choice
+{
+    public Choice() { }
+
+    [SetsRequiredMembers]
+    public Choice(Guid appointmentId, string user)
+    {
+        AppointmentId = appointmentId;
+        User = user;
+    }
+
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    public required Guid AppointmentId { get; init; }
+
+    public required string User { get; set; }
+
+    public DateTimeOffset CreationTime { get; init; } = DateTimeOffset.UtcNow;
+}
