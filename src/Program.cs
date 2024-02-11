712af0caebf4ca5516dc3df45969751874052f3c
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-02-11 12:39:03 +0100

    Register identity service

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NextTime;
 using NextTime.Components;
+using NextTime.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
 var app = builder.Build();
