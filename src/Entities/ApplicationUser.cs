85b5a471479360f6a1558666f177d009bab7a87a
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-02-11 16:51:06 +0100

    Generate random user name if argument is not provided

--- src/Entities/ApplicationUser.cs
+++ src/Entities/ApplicationUser.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Identity;
+using NextTime.Utilities;
 
 namespace NextTime.Entities;
 
 public sealed class ApplicationUser : IdentityUser<Guid>
 {
-    public ApplicationUser() : base() { }
+    public ApplicationUser() : base(UserNameGenerator.GenerateUserName()) { }
 
     public ApplicationUser(string userName) : base(userName) { }
 
