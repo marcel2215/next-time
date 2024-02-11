752d0cc87260fb7b4598df8d3753d765b36304ac
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-02-11 01:29:10 +0100

    Add Appointment entity class

---
+++ src/Entities/Appointment.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace NextTime.Entities;
+
+public sealed class Appointment
+{
+    public Appointment() { }
+
+    [SetsRequiredMembers]
+    public Appointment(string title, string? description = null, string? location = null)
+    {
+        Title = title;
+        Description = description;
+        Location = location;
+    }
+
+    public Guid Id { get; init; }
+
+    public required string Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Location { get; set; }
+
+    public DateTimeOffset CreationTime { get; init; } = DateTimeOffset.UtcNow;
+
+    public DateTimeOffset StartTime { get; set; } = DateTimeOffset.UtcNow;
+
+    public DateTimeOffset EndTime { get; set; } = DateTimeOffset.UtcNow.AddDays(30);
+}
