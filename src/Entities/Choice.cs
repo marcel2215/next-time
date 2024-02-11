319605ecc0e2ab4df53df8c533dd31317c67d308
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-02-11 12:45:45 +0100

    Remove User property from Choice class

--- src/Entities/Choice.cs
+++ src/Entities/Choice.cs
@@ -7,10 +7,9 @@ public sealed class Choice
     public Choice() { }
 
     [SetsRequiredMembers]
-    public Choice(Guid appointmentId, string user)
+    public Choice(Guid appointmentId)
     {
         AppointmentId = appointmentId;
-        User = user;
     }
 
     public Guid Id { get; init; } = Guid.NewGuid();
@@ -18,8 +17,6 @@ public sealed class Choice
     public required Guid AppointmentId { get; init; }
 
     public Appointment Appointment { get; init; } = default!;
-
-    public required string User { get; set; }
 
     public DateTimeOffset CreationTime { get; init; } = DateTimeOffset.UtcNow;
 
