5e0518cb5cd3af6bac350019ebf792ecf93d5ca8
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-02-14 12:57:52 +0100

    Unselect entire day if all hours are unselected

--- src/DataTransferObjects/SelectableDay.cs
+++ src/DataTransferObjects/SelectableDay.cs
@@ -4,6 +4,14 @@ namespace NextTime.DataTransferObjects;
 
 public sealed class SelectableDay
 {
+    private bool _isSelected;
+    private bool _is8AMSelected;
+    private bool _is10AMSelected;
+    private bool _is12PMSelected;
+    private bool _is4PMSelected;
+    private bool _is6PMSelected;
+    private bool _is8PMSelected;
+
     public SelectableDay() { }
 
     [SetsRequiredMembers]
@@ -14,19 +22,77 @@ public sealed class SelectableDay
 
     public required DateTimeOffset Time { get; set; }
 
-    public bool IsSelected { get; set; }
+    public bool IsSelected
+    {
+        get => _isSelected;
+        set
+        {
+            _isSelected = value;
+            IsLastActionSelect = value;
+        }
+    }
 
-    public bool Is8AMSelected { get; set; }
+    public bool Is8AMSelected
+    {
+        get => _is8AMSelected;
+        set
+        {
+            _is8AMSelected = value;
+            IsLastActionSelect = value;
+        }
+    }
 
-    public bool Is10AMSelected { get; set; }
+    public bool Is10AMSelected
+    {
+        get => _is10AMSelected;
+        set
+        {
+            _is10AMSelected = value;
+            IsLastActionSelect = value;
+        }
+    }
 
-    public bool Is12PMSelected { get; set; }
+    public bool Is12PMSelected
+    {
+        get => _is12PMSelected;
+        set
+        {
+            _is12PMSelected = value;
+            IsLastActionSelect = value;
+        }
+    }
 
-    public bool Is4PMSelected { get; set; }
+    public bool Is4PMSelected
+    {
+        get => _is4PMSelected;
+        set
+        {
+            _is4PMSelected = value;
+            IsLastActionSelect = value;
+        }
+    }
 
-    public bool Is6PMSelected { get; set; }
+    public bool Is6PMSelected
+    {
+        get => _is6PMSelected;
+        set
+        {
+            _is6PMSelected = value;
+            IsLastActionSelect = value;
+        }
+    }
 
-    public bool Is8PMSelected { get; set; }
+    public bool Is8PMSelected
+    {
+        get => _is8PMSelected;
+        set
+        {
+            _is8PMSelected = value;
+            IsLastActionSelect = value;
+        }
+    }
+
+    public bool IsLastActionSelect { get; set; } = true;
 
     public string FriendlyDate => Time.ToString("dddd, dd MMMM");
 }
