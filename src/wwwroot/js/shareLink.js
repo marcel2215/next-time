be755dcff7f3dd9e9da2150e23138bda987090ec
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-02-15 00:52:32 +0100

    Update shared link text

--- src/wwwroot/js/shareLink.js
+++ src/wwwroot/js/shareLink.js
@@ -1,5 +1,5 @@
-window.shareLink = function(title, url) {
-  navigator.share && navigator.share({ title: title, text: 'Check out this link!', url: url })
+window.shareLink = function (title, url) {
+  navigator.share && navigator.share({ title: title, text: 'Choose when you are available to take part in this event!', url: url })
     .then(() => console.log('Successful share'))
     .catch((error) => console.log('Error sharing', error));
 }
