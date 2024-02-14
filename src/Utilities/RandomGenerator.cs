e93c8608934d10e4bdb76abb344d9a05eee48a5c
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-02-14 13:36:03 +0100

    Add method to generate random meeting code

--- src/Utilities/RandomGenerator.cs
+++ src/Utilities/RandomGenerator.cs
@@ -8,4 +8,9 @@ public static class RandomGenerator
     {
         return RandomNumberGenerator.GetString("abcdefghijklmnopqrstuvwxyz0123456789", 16);
     }
+
+    public static string GenerateMeetingCode()
+    {
+        return RandomNumberGenerator.GetString("abcdefghijklmnopqrstuvwxyz0123456789", 8);
+    }
 }
